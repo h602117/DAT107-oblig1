3a20864ea635cdafddf9534c70972c0db9bdd423
Anders Fimreite <anders.fimreite@gmail.com>
2022-01-28 13:07:20 +0100

    skal fungere no

--- create.sql
+++ create.sql
@@ -1,44 +1,75 @@
--- a)
-drop table if exists person;
-drop table if exists car;
-drop table if exists toll_booth;
-drop table if exists passes;
+-----------------------------------
+-- Create fresh tables ------------
+-----------------------------------
 
--- Tabell med personopplysninger:
---    Valgte aa lage egen tabell til dette i steden for aa ha det i car tabellen
-create table person (
-    id integer primary key,
-    first_name varchar not null,
-    last_name varchar not null,
-    adress varchar unique,
-    email varchar unique,
-    phone_number integer unique
+DROP TABLE IF EXISTS person CASCADE;
+DROP TABLE IF EXISTS car CASCADE;
+DROP TABLE IF EXISTS toll_booth CASCADE;
+DROP TABLE IF EXISTS passing CASCADE;
+
+CREATE TABLE person (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  address VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  phone INTEGER NOT NULL
 );
 
--- Tabell med bilopplysninger:
---     Refererer til person tabellen
-create table car (
-    id integer primary key,
-    license varchar,
-    owner_id integer not null,
-    constraint ownerFK foreign key (owner_id) references person (id)
+CREATE TABLE car (
+  license_plate VARCHAR PRIMARY KEY,
+  owner_id INTEGER NOT NULL,
+  FOREIGN KEY (owner_id)
+    REFERENCES person (id)
+    ON DELETE CASCADE
 );
 
--- Tabell med bomopplysninger:
---     Valgte aa opprette en egen tabell til dette i steden for aa ha den i passes tabellen
-create table toll_booth (
-    id integer primary key,
-    loc varchar not null
+CREATE TABLE toll_booth (
+  id SERIAL PRIMARY KEY,
+  location VARCHAR NOT NULL
 );
 
--- Tabell med alle passeringer: 
---     Refererer til car tabellen
---     Refererer til toll_booth tabellen
-create table passes (
-    id integer primary key,
-    time_stamp timestamp not null,
-    toll_booth_id integer not null,
-    car_id integer not null,
-    constraint toll_boothFK foreign key (toll_booth_id) references toll_booth (id),
-    constraint carFK foreign key (car_id) references car (id)
+CREATE TABLE passing (
+  id SERIAL PRIMARY KEY,
+  time TIMESTAMP NOT NULL,
+  car_license_plate VARCHAR,
+  toll_booth_id INTEGER NOT NULL,
+  FOREIGN KEY (car_license_plate)
+    REFERENCES car (license_plate)
+    ON DELETE CASCADE,
+  FOREIGN KEY (toll_booth_id)
+    REFERENCES toll_booth (id)
+    ON DELETE CASCADE
 );
+
+-----------------------------------
+-- Insert test data ---------------
+-----------------------------------
+
+INSERT INTO person (name, address, email, phone)
+VALUES  ('Test Testesen',  'Testegata 1',    'test.testesen@test.com',  12345678),
+        ('Ola Nordmann',   'Norskegata 17',  'ola.nordmann@norge.no',   17051814),
+        ('Kari Nordmann',  'Norskegata 17',  'kari.nordmann@norge.no',  87654321),
+        ('Ivar Aasen',     'Ørsta',          'ivar.aasen@nynorsk.no',   99999999);
+
+INSERT INTO car (license_plate, owner_id)
+VALUES  ('UC12345', 1),
+        ('TV12345', 2),
+        ('VJ12345', 3),
+        ('AA10000', 4);
+
+INSERT INTO toll_booth (location)
+VALUES  ('Oslo'),
+        ('Trondheim'),
+        ('Førde'),
+        ('Sogndal');
+
+INSERT INTO passing (time, car_license_plate, toll_booth_id)
+VALUES  (NOW()::timestamp + interval '1 hours', 'UC12345', 4),
+        (NOW()::timestamp + interval '2 hours', 'TV12345', 4),
+        (NOW()::timestamp + interval '3 hours', 'VJ12345', 2),
+        (NOW()::timestamp + interval '4 hours', 'AA10000', 1),
+        (NOW()::timestamp + interval '5 hours', 'AA10000', 2),
+        (NOW()::timestamp + interval '5 hours', 'TV12345', 1),
+        (NOW()::timestamp + interval '6 hours', 'TV12345', 3),
+        (NOW()::timestamp + interval '7 hours', 'TV12345', 2),
+        (NOW()::timestamp + interval '8 hours', NULL, 2);
--- querys.sql
+++ querys.sql
@@ -1,6 +1,46 @@
 -- b)
-select 
-    k.id, k.time_stamp, k.toll_booth_id, t.loc, k.car_id, c.license,
-    c.owner_id, p.first_name, p.last_name, p.adress, p.email, p.phone_number 
-from person p, car c, toll_booth t, passes k
-where k.car_id = c.id and k.toll_booth_id = t.id and c.owner_id = p.id;
+SELECT
+  p.id, p.time, t.id as tb_id, t.location, c.license_plate,
+  o.id as owner_id, o.name, o.address, o.email, o.phone
+FROM passing p
+FULL JOIN
+(
+car c INNER JOIN person o
+ON o.id = c.owner_id
+)
+ON c.license_plate = p.car_license_plate
+INNER JOIN toll_booth t
+ON t.id = p.toll_booth_id;
+
+-- c)
+SELECT
+  p.id, p.time, t.id as tb_id, t.location, c.license_plate,
+  o.id as owner_id, o.name, o.address, o.email, o.phone
+FROM passing p
+INNER JOIN
+(
+car c INNER JOIN person o
+ON o.id = c.owner_id
+)
+ON c.license_plate = p.car_license_plate
+INNER JOIN toll_booth t
+ON t.id = p.toll_booth_id;
+
+-- e)
+SELECT car_license_plate, COUNT(car_license_plate) as count_passings
+FROM passing
+GROUP BY car_license_plate
+HAVING car_license_plate IS NOT NULL;
+
+-- f)
+-- Tolket som finn tidspunkt for siste passering av bil med reg.nr. AA10000
+SELECT car_license_plate, time as last_passing
+FROM passing
+WHERE car_license_plate = 'AA10000'
+ORDER BY time DESC LIMIT 1;
+
+-- g)
+SELECT COUNT(id) as count_unreadable
+FROM passing
+GROUP BY car_license_plate
+HAVING car_license_plate IS NULL;
